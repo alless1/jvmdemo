c6c00506427e82b22cbab247b316419e4a6ad8a3
chengjie <chengjie0521@ubox.cn>
2020-03-17 20:41:19 +0800

    master add 1

---
+++ libTestClassload/src/main/java/com/example/memory/MyTest4.java
@@ -0,0 +1,13 @@
+package com.example.memory;
+
+import java.sql.SQLOutput;
+
+/**
+ * Created by chengjie on 2020-03-17
+ * Description:
+ */
+public class MyTest4 {
+    public static void main(String[] args) {
+        System.out.println("master add 1");
+    }
+}
