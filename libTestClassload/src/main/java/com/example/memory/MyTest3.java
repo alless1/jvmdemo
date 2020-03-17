689b4f8a08db34d701495ab6c3e3d9654a7b9e88
chengjie <chengjie0521@ubox.cn>
2020-03-17 19:49:54 +0800

    ss

---
+++ libTestClassload/src/main/java/com/example/memory/MyTest3.java
@@ -0,0 +1,11 @@
+package com.example.memory;
+
+/**
+ * Created by chengjie on 2020-03-17
+ * Description:
+ */
+public class MyTest3 {
+    public static void main(String[] args) {
+        System.out.println("add branch dev ");
+    }
+}
